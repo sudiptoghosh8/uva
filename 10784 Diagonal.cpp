d3dac6bc4a862cfbec4d953b0a8a910aff1fdf4a
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2020-11-01 00:20:33 +0600

    Add files via upload

---
+++ 10784 Diagonal.cpp
@@ -0,0 +1,13 @@
+ #include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+   double n,p;
+for(i=1;scanf("%lf",&n) && n;i++){
+    p = ceil((3+sqrt(9+8*n))/2);
+printf("Case %d: %.0lf\n",i,p);
+
+}
+    return 0;
+}
