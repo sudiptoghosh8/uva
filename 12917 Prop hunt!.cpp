30c6b19f87655395bf57234dede6c58ea48358a8
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2020-12-18 11:30:55 -0500

    Add files via upload

---
+++ 12917 Prop hunt!.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int a,b,c;
+
+   while(scanf("%d%d%d",&a,&b,&c)==3)
+    {
+
+    if(a<=(c-b))
+    {
+        cout<<"Props win!"<<endl;
+    }
+    else
+        {cout<<"Hunters win!"<<endl;
+    }
+
+    }
+
+return 0;
+}
