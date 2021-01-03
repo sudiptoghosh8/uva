b6526f3d580222dc8a80c01af7e6848cdd843e4d
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-01-03 11:14:51 +0600

    Add files via upload

---
+++ 10082 WERTYU.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include<string.h>
+
+using namespace std;
+int main()
+{
+    char s;
+    while ( scanf("%c",&s)==1)
+    {
+        if (s=='1')
+            printf("`");
+        if (s=='2')
+            printf("1");
+        if (s=='3')
+            printf("2");
+        if (s=='4')
+            printf("3");
+        if (s=='5')
+            printf("4");
+        if (s=='6')
+            printf("5");
+        if (s=='7')
+            printf("6");
+        if (s=='8')
+            printf("7");
+        if (s=='9')
+            printf("8");
+        if (s=='0')
+            printf("9");
+        if (s=='-')
+            printf("0");
+        if (s=='=')
+            printf("-");
+        if (s=='W')
+            printf("Q");
+        if (s=='E')
+            printf("W");
+        if (s=='R')
+            printf("E");
+        if (s=='T')
+            printf("R");
+        if (s=='Y')
+            printf("T");
+        if (s=='U')
+            printf("Y");
+        if (s=='I')
+            printf("U");
+        if (s=='O')
+            printf("I");
+        if (s=='P')
+            printf("O");
+        if (s=='[')
+            printf("P");
+        if (s==']')
+            printf("[");
+        if (s=='\\')
+            printf("]");
+        if (s=='S')
+            printf("A");
+        if (s=='D')
+            printf("S");
+        if (s=='F')
+            printf("D");
+        if (s=='G')
+            printf("F");
+        if (s=='H')
+            printf("G");
+        if (s=='J')
+            printf("H");
+        if (s=='K')
+            printf("J");
+        if (s=='L')
+            printf("K");
+        if (s==';')
+            printf("L");
+        if (s=='\'')
+            printf(";");
+        if (s=='X')
+            printf("Z");
+        if (s=='C')
+            printf("X");
+        if (s=='V')
+            printf("C");
+        if (s=='B')
+            printf("V");
+        if (s=='N')
+            printf("B");
+        if (s=='M')
+            printf("N");
+        if (s==',')
+            printf("M");
+        if (s=='.')
+            printf(",");
+        if (s=='/')
+            printf(".");
+        if (s==' ')
+            printf(" ");
+        if (s=='\n')
+            printf("\n");
+    }
+    return 0;
+}
