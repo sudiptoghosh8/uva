3986d2ff4409e9cba9fae531b364623ff19e3052
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-03-13 03:06:29 +0600

    Add files via upload

---
+++ 13148  A Giveaway.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    unsigned long long int num;
+    while(scanf("%ulld",&num)==1)
+    {
+    {
+        if(num==1|| num==64|| num==729|| num==4096|| num==15625|| num==46656|| num== 117649|| num==262144|| num==531441||
+           num==1000000|| num==1771561|| num==2985984|| num==4826809|| num==7529536|| num==11390625|| num==16777216|| num==24137569|| num==34012224|| num==
+47045881|| num==64000000 || num==85766121
+)
+   {
+   cout<<"Special"<<endl;
+    }
+    else {
+        cout<<"Ordinary"<<endl;
+    }
+    }
+
+}
+return 0;
+}
