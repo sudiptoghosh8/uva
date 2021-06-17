327ebd7651531acaf482be6e912a8f9114dcbf4a
Sudipto Ghosh <70814428+sudiptoghosh8@users.noreply.github.com>
2021-06-17 23:24:42 +0600

    Add files via upload

---
+++ UVA-11764 Jumping Mario.cpp
@@ -0,0 +1,36 @@
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[100],i,j,t,n,k,h,l;
+    cin>>t;
+    for(i=1;i<=t;i++)
+    {
+        h=0;
+        l=0;
+        cin>>n;
+        for(j=1;j<=n;j++)
+        {
+            cin>>a[j];
+
+        }
+        for(j=1;j<=n-1;j++)
+        {
+            if(a[j]<a[j+1])
+            {
+                h++;
+            }
+            else if(a[j]>a[j+1])
+            {
+                l++;
+            }
+        }
+        printf("Case %d: %d %d\n",i,h,l);
+
+    }
+    return 0;
+}
